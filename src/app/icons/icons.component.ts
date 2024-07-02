f78917173d8ebaa49535aa23408b7c74813b9e8e
Binshabs <binshabs567@gmail.com>
2024-07-02 11:12:28 +0530

    css-updation

--- src/app/icons/icons.component.ts
+++ src/app/icons/icons.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class IconsComponent implements OnInit  {
 
   icons: string[] = [
-    'fa fa-album', 'fa fa-arc', 'fa fa-arrow-left', 'fa fa-band-aid',
-    'fa fa-car', 'fa fa-diamond', 'fa fa-door-lock', 'fa fa-eyedropper',
+     'fa fa-arrow-left', 'fa fa-band-aid',
+    'fa fa-car', 'fa fa-diamond', 'fa fa-eyedropper',
     'fa fa-female', 'fa fa-dumbbell', 'fa fa-hammer', 'fa fa-headphones',
     'fa fa-heart', 'fa fa-home', 'fa fa-leaf', 'fa fa-music',
     'fa fa-paint-brush', 'fa fa-paper-plane', 'fa fa-rocket', 'fa fa-star',
